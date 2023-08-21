2a7e17bcf353ef4debf5e3c1b385cbdd3f9c7bda
Ryenook <piaolianyu96@naver.com>
2023-08-21 11:14:59 +0900

    동적 정적

--- src/main/java/com/example/appec2/TestController.java
+++ src/main/java/com/example/appec2/TestController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
+/*@ResponseBody  // 리턴값을 문자열로 나오게 하고싶을때는 사용하나 파일로 연결 시킬때는 주석*/
 @RequestMapping("/web")
 public class TestController {
 
     @GetMapping("/nice")
-    public String hi(){
+    public String nice(){
         return "nice";
     }
 
